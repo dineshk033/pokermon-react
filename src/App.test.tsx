fe117e5bad2ed23cbc9263b36ed5b2a664ea46cb
dineshk033 <dineshk033@gmail.com>
2021-10-03 12:13:10 +0530

    Enzyme integrated

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Project/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrrapper = shallow(<App/>);
+ console.log(wrrapper)
+  expect(wrrapper).toMatchSnapshot();
 });
