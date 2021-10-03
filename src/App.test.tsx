a4d6c3199cbd07b950579da7fe660552edc17938
dineshk033 <dineshk033@gmail.com>
2021-10-03 12:22:15 +0530

    Enzyme integrated for testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import App from './App';
 
 test('renders learn react link', () => {
-  const wrrapper = shallow(<App/>);
- console.log(wrrapper)
+  const wrrapper = shallow(<App />);
+  console.log(wrrapper);
   expect(wrrapper).toMatchSnapshot();
 });
